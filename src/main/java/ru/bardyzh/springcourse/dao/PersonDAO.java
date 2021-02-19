3ac88b172fddf534309e2c5ba94e1ad6457203b7
Arseni_Bardij <senia_94@mail.ru>
2021-02-19 17:28:52 +0300

    Lesson 24

--- src/main/java/ru/bardyzh/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/bardyzh/springcourse/dao/PersonDAO.java
@@ -5,6 +5,8 @@ import ru.bardyzh.springcourse.models.Person;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
@@ -12,11 +14,13 @@ public class PersonDAO {
     public List<Person> people;
 
     {
+        Random random = new Random();
+        int age = random.nextInt(100);
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT,"Tom"));
-        people.add(new Person(++PEOPLE_COUNT,"Bill"));
-        people.add(new Person(++PEOPLE_COUNT,"Jack"));
-        people.add(new Person(++PEOPLE_COUNT,"Mark"));
+        people.add(new Person(++PEOPLE_COUNT,"Tom", age, "1@1.tv"));
+        people.add(new Person(++PEOPLE_COUNT,"Bill", age, "1@1.tv"));
+        people.add(new Person(++PEOPLE_COUNT,"Jack", age, "1@1.tv"));
+        people.add(new Person(++PEOPLE_COUNT,"Mark", age, "1@1.tv"));
     }
 
     public List<Person> index(){
@@ -36,6 +40,8 @@ public class PersonDAO {
         Person personToBeUpdated = show(id);
 
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id){
